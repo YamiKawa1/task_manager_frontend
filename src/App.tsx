b13830d7ff22105e63f77107c36103a98df7c001
Gian vespa <givespas@gmail.com>
2023-01-13 17:03:39 -0500

    refactor: use api + use state global

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+import { TaskObject } from './interface';
+import { getTasks } from './API';
+
+import CreateTaskForm from './Components/TaskList/CreateTaskForm';
 import ArchiveSwitch from './Components/NavBar/ArchiveSwitch';
 import ListTask from './Components/TaskList/ListTask';
 
-const App: React.FC = () => {
+export function App() {
+  const [tasks, setTasks] = useState<Array<TaskObject>>([]);
+  const [doneTasks, setDoneTasks] = useState<Array<TaskObject>>([]);
+  const [archivedTasks, setArchivedTasks] = useState<Array<TaskObject>>([]);
+
+  useEffect(() => {
+    loadingTask();
+  }, []);
+
+  const loadingTask = async () => {
+    const res = await getTasks();
+    console.log(res);
+    const doneTasks: Array<TaskObject> = [];
+    const archivedTasks: Array<TaskObject> = [];
+    const restTask: Array<TaskObject> = [];
+
+    res.forEach((task: TaskObject) => {
+      if (task.done) {
+        doneTasks.push(task);
+      } else if (!task.done && task.archived) {
+        archivedTasks.push(task);
+      } else if (!task.archived && !task.done) {
+        restTask.push(task);
+      }
+    });
+    setTasks(restTask);
+    setDoneTasks(doneTasks);
+    setArchivedTasks(archivedTasks);
+  };
+
   return (
     <div className="App container">
-      <ArchiveSwitch/>
-      <ListTask/>
+      <ArchiveSwitch />
+      <CreateTaskForm />
+      <ListTask
+        tasks={tasks}
+        doneTasks={doneTasks}
+        archivedTasks={archivedTasks}
+      />
     </div>
   );
 }
-
-export default App;
