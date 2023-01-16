b8477b453d3a82a378edfcebd2043f450b841fe8
Jehan Veracierta <jehanzao@gmail.com>
2023-01-16 15:49:53 -0400

    feat: pasar estado global

--- src/Components/TaskList/ListTask.tsx
+++ src/Components/TaskList/ListTask.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import Task from './Task';
-import { TaskObject } from '../../interface';
+import { TaskObject, StateInfoObject } from '../../interface';
 
 interface Props {
   tasks: Array<TaskObject>;
   doneTasks: Array<TaskObject>;
   archivedTasks: Array<TaskObject>;
+  loading:any;
+  setShow:any;
+  stateInfo:StateInfoObject;
 }
 
-function ListTask({ tasks, doneTasks, archivedTasks }: Props): JSX.Element {
+function ListTask({ tasks, doneTasks, archivedTasks, loading, setShow, stateInfo }: Props): JSX.Element {
   return (
     <div>
-      <Task TaskInfo={tasks} color="warning" />
-      <Task TaskInfo={doneTasks} color="success" />
-      <Task TaskInfo={archivedTasks} color="danger" />
+      <Task TaskInfo={tasks} color="warning" loading={loading} setShow={setShow} stateInfo={stateInfo}/>
+      <Task TaskInfo={doneTasks} color="success" loading={loading} setShow={setShow} stateInfo={stateInfo}/>
+      <Task TaskInfo={archivedTasks} color="danger" loading={loading} setShow={setShow} stateInfo={stateInfo}/>
     </div>
   );
 }
