1e5603e42ae5e53a6b42d7c6fde644abf82718ba
Jehan Veracierta <jehanzao@gmail.com>
2023-01-16 15:47:46 -0400

    feat: estado global boton + de crear task

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,43 @@ import { getTasks } from './API';
 import CreateTaskForm from './Components/TaskList/CreateTaskForm';
 import ArchiveSwitch from './Components/NavBar/ArchiveSwitch';
 import ListTask from './Components/TaskList/ListTask';
+import { Button } from 'react-bootstrap';
+import { StateInfoObject } from './interface';
 
 export function App() {
+  const [show, setShow] = useState<boolean>(false);
+  const [edit, setEdit] = useState<boolean>(false);
   const [tasks, setTasks] = useState<Array<TaskObject>>([]);
   const [doneTasks, setDoneTasks] = useState<Array<TaskObject>>([]);
   const [archivedTasks, setArchivedTasks] = useState<Array<TaskObject>>([]);
+  const [id, setId] = useState<number>(NaN);
+  const [title, setTitle] = useState<string>('');
+  const [info, setInfo] = useState<string>('');
+  const [date, setDate] = useState<string>('');
+  const [complexity, setComplexity] = useState<string>('');
+
+
+
+  const stateInfo:StateInfoObject = {
+    id: id,
+    setId: setId,
+    title: title,
+    setTitle: setTitle,
+    info: info,
+    setInfo: setInfo,
+    date: date,
+    setDate: setDate,
+    complexity: complexity,
+    setComplexity: setComplexity,
+    edit: edit,
+    setEdit: setEdit, 
+  }
 
   useEffect(() => {
     loadingTask();
   }, []);
+
+  const handleShow = () => setShow(true);
 
   const loadingTask = async () => {
     const res = await getTasks();
@@ -40,11 +68,23 @@ export function App() {
   return (
     <div className="App container">
       <ArchiveSwitch />
-      <CreateTaskForm loading={loadingTask} />
+      <Button variant="success" onClick={handleShow}>
+        Create New Task
+      </Button>
+
+      <CreateTaskForm 
+      loading={loadingTask}
+      show={show} 
+      setShow={setShow}
+      stateInfo={stateInfo}
+       />
       <ListTask
+        setShow={setShow}
         tasks={tasks}
         doneTasks={doneTasks}
         archivedTasks={archivedTasks}
+        loading={loadingTask}
+        stateInfo={stateInfo}
       />
     </div>
   );
