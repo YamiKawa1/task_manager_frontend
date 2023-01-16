74f6798e7e2dd0bb38af8fc060fdcd04a1f26006
Jehan Veracierta <jehanzao@gmail.com>
2023-01-16 15:48:41 -0400

    feat: peticiones axios

--- src/API/tasks.ts
+++ src/API/tasks.ts
@@ -6,6 +6,13 @@ export const getTasks = async () => {
   return res.data.data;
 };
 
+export const getTaskById = async (id: number) => {
+  const res = await axios.get(`${process.env.REACT_APP_URL_BACKEND}/tasks/${id}`);  
+  console.log(res);
+  
+  return res.data.data;
+}
+
 export const postCreateTask = async (body: TaskCreate) => {
   const res = await axios.post(
     `${process.env.REACT_APP_URL_BACKEND}/tasks`,
@@ -13,3 +20,26 @@ export const postCreateTask = async (body: TaskCreate) => {
   );
   return res.data.data;
 };
+
+export const patchArchiveTask = async(id: number) => {
+  const baseURL = `${process.env.REACT_APP_URL_BACKEND}/tasks/archive/${id}`;
+  const res = await axios.patch(baseURL)
+
+  return res.data
+}
+
+export const patchDoneTask = async(id: number) => {
+  const baseURL = `${process.env.REACT_APP_URL_BACKEND}/tasks/done/${id}`;
+  const res = await axios.patch(baseURL)
+
+  return res.data
+}
+
+export const patchUpdateTask = async(id:number, body:TaskCreate) => {
+  const res = await axios.patch(
+    `${process.env.REACT_APP_URL_BACKEND}/tasks/update/${id}`,
+    body
+  );
+
+  return res.data.data;
+}
