6c723d92104e3dd62ffb95284c1656dd122441cd
Jehan Veracierta <jehanzao@gmail.com>
2023-01-16 15:50:47 -0400

    feat: estado global

--- src/Components/TaskList/Task.tsx
+++ src/Components/TaskList/Task.tsx
@@ -1,14 +1,18 @@
+import { render } from '@testing-library/react';
 import axios from 'axios';
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import * as Icon from 'react-bootstrap-icons';
+import { patchArchiveTask, patchDoneTask, getTaskById} from '../../API'
+import { StateInfoObject } from '../../interface';
+import CreateTaskForm from './CreateTaskForm';
 
 interface Props {
   TaskInfo: Array<{
     id: number;
     title?: string;
     information?: string;
-    task_date?: Date;
+    task_date?: string;
     complexity?: string;
     done: boolean;
     archived: boolean;
@@ -16,25 +20,48 @@ interface Props {
     updated_at?: Date;
   }>;
   color: string;
+  loading: any;
+  setShow:any;
+  stateInfo: StateInfoObject;
 }
 
-const archiveTask = (id: number): any => {
-  const baseURL = `http://127.0.0.1:3333/tasks/archive/${id}`;
-  console.log(id);
-  axios.patch(baseURL).then((response) => {
-    console.log(response.data.message);
-  });
-};
-
-const editTask = (id: number): any => {};
-
-const getTime = (task_date?: Date): string => {
+const getTime = (task_date?: string): string => {
   const actual = Date();
   // console.log(task_date, actual)
   return '';
 };
 
-const Task = ({ TaskInfo, color }: Props) => {
+const Task = ({ TaskInfo, color, loading, setShow, stateInfo }: Props) => {
+
+  const handleShow = () => setShow(true);
+
+  const archiveTask: any = async (id: number) => {
+    const res = await patchArchiveTask(id);
+    console.log(res.message);
+    loading();
+  };
+  
+  const doneTask: any = async (id: number) => {
+    const res = await patchDoneTask(id);
+    console.log(res.message);
+    loading();
+  };
+
+  const editTask: any = async (id: number) => {
+    stateInfo.setEdit(true);
+    const res = await getTaskById(id);
+    
+    stateInfo.setId(id);
+    stateInfo.setTitle(res.title);
+    stateInfo.setInfo(res.information);
+    stateInfo.setDate(res.task_date);
+    stateInfo.setComplexity(res.complexity);
+
+    handleShow()
+  };
+
+
+
   return (
     <ul className="d-inline-flex flex-wrap">
       {TaskInfo.map((task) => {
@@ -44,22 +71,34 @@ const Task = ({ TaskInfo, color }: Props) => {
             className={`position-relative bg-${color} bg-opacity-10`}
           >
             <Button
+              variant="success"
+              data-key={task.id}
+              onClick={(e) => doneTask(task.id)}
+              className={`position-absolute top-0 ${task.done ? 'end-0 m-1' : 'mt-1 ms-4'}`}
+            >
+              <Icon.FolderFill size={15} />
+            </Button>
+            
+            {task.done 
+            ? null
+            : <Button
               variant="danger"
               data-key={task.id}
               onClick={(e) => archiveTask(task.id)}
               className="position-absolute top-0 end-0 m-1"
-            >
-              <Icon.TrashFill size={15} />
-            </Button>
+              >
+                <Icon.TrashFill size={15} />
+              </Button> 
+              }
             <Button
               variant="warning"
               data-key={task.id}
-              onClick={(e) => editTask(task.id)}
+              onClick={e=> editTask(task.id)}
               className="position-absolute bottom-0 end-0 m-1"
-            >
-              <Icon.PencilFill size={15} />
+            ><Icon.PencilFill size={15} />
             </Button>
-            <h6 className="text-start ms-2 mt-1">{task.title}</h6>
+              
+            <h6 className="text-start ms-2 mt-1">{task.title?.substring(0, 20)}...</h6>
             <p className="text-start ms-2">
               {task.information?.substring(0, 20)}...
             </p>
