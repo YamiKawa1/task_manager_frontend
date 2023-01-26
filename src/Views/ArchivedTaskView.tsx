c3ff379a6101e9aabcbfe140d6b31a9eaad2e16e
Jehan Veracierta <jehanzao@gmail.com>
2023-01-26 15:32:04 -0400

    feat: vistas para todo, done and archived task

---
+++ src/Views/ArchivedTaskView.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ArchivedTaskView = () => {
+  return (
+<div>
+
+</div>
+  );
+}
+
+export default ArchivedTaskView
---
+++ src/Views/ToDoDoneTasksView.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Task from '../Components/TaskList/Task'
+import DoneTask from '../Components/TaskList/DoneTask'
+
+const ToDoDoneTasksView = () => {
+  return (
+    <div>
+      {/* <Task></Task>
+      <DoneTask></DoneTask> */}
+    </div>
+  )
+}
+
+export default ToDoDoneTasksView
