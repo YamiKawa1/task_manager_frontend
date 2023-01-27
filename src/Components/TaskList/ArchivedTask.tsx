389dcb56f87a03d3db80025c81dc36cd7062b843
Jehan Veracierta <jehanzao@gmail.com>
2023-01-26 20:43:56 -0400

    Archivedtask componente completado

--- src/Components/TaskList/ArchivedTask.tsx
+++ src/Components/TaskList/ArchivedTask.tsx
@@ -1,64 +1,17 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import * as Icon from 'react-bootstrap-icons';
-import { patchArchiveTask, patchDoneTask, getTaskById} from '../../API'
-import { StateInfoObject } from '../../interface';
-import { transformDate } from '../../Helpers';
+import { TaskObject } from '../../interface';
+import { deleteTask, restoreTask } from '../../features/tasks/taskSlice';
+import { useDispatch } from 'react-redux';
 
 interface Props {
-  TaskInfo: Array<{
-    id: number;
-    title?: string;
-    information?: string;
-    task_date?: string;
-    complexity?: string;
-    done: boolean;
-    archived: boolean;
-    created_at?: Date;
-    updated_at?: Date;
-  }>;
+  TaskInfo: Array<TaskObject>;
   loading: any;
-  setShow:any;
-  stateInfo: StateInfoObject;
 }
 
-const getTime = (task_date?: string): string => {
-  const actual = Date();
-  // console.log(task_date, actual)
-  return '';
-};
-
-const ArchivedTask = ({ TaskInfo, loading, setShow, stateInfo }: Props) => {
-
-  const handleShow = () => setShow(true);
-
-  const archiveTask: any = async (id: number) => {
-    const res = await patchArchiveTask(id);
-    console.log(res.message);
-    loading();
-  };
-  
-  const doneTask: any = async (id: number) => {
-    const res = await patchDoneTask(id);
-    console.log(res.message);
-    loading();
-  };
-
-  const editTask: any = async (id: number) => {
-    stateInfo.setEdit(true);
-    const res = await getTaskById(id);
-    console.log(transformDate(res.task_date));
-    
-    stateInfo.setId(id);
-    stateInfo.setTitle(res.title);
-    stateInfo.setInfo(res.information);
-    stateInfo.setDate(transformDate(res.task_date));
-    stateInfo.setComplexity(res.complexity);
-
-    handleShow()
-  };
-
-
+const ArchivedTask = ({ TaskInfo, loading }: Props) => {
+  const dispatch = useDispatch();
 
   return (
     <ul className="d-inline-flex flex-wrap">
@@ -70,38 +23,28 @@ const ArchivedTask = ({ TaskInfo, loading, setShow, stateInfo }: Props) => {
           >
             <Button
               variant="success"
-              data-key={task.id}
-              onClick={(e) => doneTask(task.id)}
-              className={`position-absolute top-0 ${task.done ? 'end-0 m-1' : 'mt-1 ms-4'}`}
+              onClick={(e) => dispatch(restoreTask(task.id))}
+              className="position-absolute bot-100 end-0 m-1"
             >
-              <Icon.FolderFill size={15} />
+              <Icon.Recycle size={15} />
             </Button>
             
             {task.done 
             ? null
             : <Button
               variant="danger"
-              data-key={task.id}
-              onClick={(e) => archiveTask(task.id)}
-              className="position-absolute top-0 end-0 m-1"
+              onClick={(e) => dispatch(deleteTask(task.id))}
+              className="position-absolute bottom-0 end-0 m-1"
               >
                 <Icon.TrashFill size={15} />
               </Button> 
               }
-            <Button
-              variant="warning"
-              data-key={task.id}
-              onClick={e=> editTask(task.id)}
-              className="position-absolute bottom-0 end-0 m-1"
-            ><Icon.PencilFill size={15} />
-            </Button>
-              
             <h6 className="text-start ms-2 mt-1">{task.title?.substring(0, 15)}...</h6>
             <p className="text-start ms-2">
               {task.information?.substring(0, 20)}...
             </p>
             <p className="text-start text-muted ms-2">
-              falta: {getTime(task.task_date)}
+              Archivada
             </p>
           </li>
         );
