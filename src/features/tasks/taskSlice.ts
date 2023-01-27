70651080df1a089ae9d3827ddb531dbf8d165734
Jehan Veracierta <jehanzao@gmail.com>
2023-01-26 21:29:01 -0400

    feat: funciones del estado de las tasks

--- src/features/tasks/taskSlice.ts
+++ src/features/tasks/taskSlice.ts
@@ -9,37 +9,31 @@ export interface TaskState {
 const initialState: TaskState = {
   value: [
     {
-        id: 1,
+        id: '1',
         title: 'hola',
         information: 'in client task',
         task_date: '28/01/2023',
         complexity: 'facil',
         done: true,
         archived: false,
-        created_at: new Date(),
-        updated_at: new Date()
     },
     {
-        id: 2,
+        id: '2',
         title: 'hola 2',
         information: 'in client task 2',
         task_date: '28/01/2023',
         complexity: 'medio',
         done: false,
         archived: true,
-        created_at: new Date(),
-        updated_at: new Date()
     },
     {
-        id: 3,
+        id: '3',
         title: 'hola 3',
         information: 'in client task 3',
         task_date: '28/01/2023',
         complexity: 'dificil',
         done: false,
         archived: false,
-        created_at: new Date(),
-        updated_at: new Date()
     }
 ]
 }
@@ -48,12 +42,60 @@ export const TaskSlice = createSlice({
   name: 'tasks',
   initialState,
   reducers: {
-    
+    addTask: (state, action: PayloadAction<TaskObject>) => {
+      state.value.push(action.payload);
+    },
+
+    restoreTask: (state, action: PayloadAction<string>)=> {
+      let task = state.value.find(x => x.id === action.payload)
+      if (task) {
+        state.value[state.value.indexOf(task)].archived = false;
+      }
+    },
+
+    archiveTask: (state, action: PayloadAction<string>)=> {
+      let task = state.value.find(x => x.id === action.payload)
+      if (task) {
+        state.value[state.value.indexOf(task)].archived = true;
+      }
+    },
+
+    doneTask: (state, action: PayloadAction<string>)=> {
+      let task = state.value.find(x => x.id === action.payload)
+      if (task) {
+        state.value[state.value.indexOf(task)].done = true;
+      }
+    },
+
+    undoneTask: (state, action: PayloadAction<string>)=> {
+      let task = state.value.find(x => x.id === action.payload)
+      if (task) {
+        state.value[state.value.indexOf(task)].done = false;
+      }
+    },
+
+    deleteTask: (state, action: PayloadAction<string>)=> {      
+      let task = state.value.find(x => x.id === action.payload)
+      if (task) {
+        state.value.splice(state.value.indexOf(task), 1);
+      }
+    },
+
+    editTask: (state, action: PayloadAction<TaskObject>)=> {
+      let task = state.value.find(x => x.id === action.payload.id)
+      if (task) {
+        let taskObject = state.value[state.value.indexOf(task)]
+        taskObject.title = action.payload.title;
+        taskObject.information = action.payload.information;
+        taskObject.complexity = action.payload.complexity;
+        taskObject.task_date = action.payload.task_date;
+      }
+    },
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { } = TaskSlice.actions
+export const { addTask, deleteTask, restoreTask, undoneTask, doneTask, archiveTask} = TaskSlice.actions
 
 export default TaskSlice.reducer
 
