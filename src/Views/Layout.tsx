c2681272190ccbdc5e91bf41e0ccd983a903f470
Jehan Veracierta <jehanzao@gmail.com>
2023-01-30 08:16:35 -0400

    anadir el comp TaskCardInfo

--- src/Views/Layout.tsx
+++ src/Views/Layout.tsx
@@ -3,12 +3,11 @@ import { Button } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import CreateTaskForm from '../Components/TaskList/CreateTaskForm';
 import { setShow } from '../features/tasks/GlobalSlice';
-
+import TaskCardInfo from '../Components/TaskList/TaskCardInfo';
 
 const Layout = () => {
     const dispatch = useDispatch()
     let location = window.location.href.substring(22);
-    console.log(location);
     function generateLink() {
         if (location === 'archived') {
             return (<a href="tododone" className='color-w'>Go to Tasks</a>)
@@ -26,6 +25,7 @@ const Layout = () => {
         Create New Task
     </Button>
     <CreateTaskForm />
+    <TaskCardInfo />
     </div>
   )
 }
